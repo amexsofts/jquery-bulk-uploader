009bcc092383888ab15bf251c8881dd0fbf0fb69
Amanu <negaamanuel@gmail.com>
2015-08-01 01:28:23 +0300

    Created UI Creation up uploader, finish the handlers

--- js/uploader.js
+++ js/uploader.js
@@ -18,20 +18,20 @@
         max: 4,
         placeHolder: "../images/placeholder",
         addButtonClass: 'btn btn-sm btn-success uploader-add',
-        addButtonText: '<i class="fa fa-plus"></i> Add',
+        addButtonText: '<i class="fa fa-plus"></i>',
         saveAllButtonClass: 'btn btn-sm btn-primary',
         saveAllButtonText: '<i class="fa fa-save"></i>',
         removeAllButtonClass: 'btn btn-sm btn-danger',
         removeAllButtonText: '<i class="fa fa-remove"></i>',
-        saveButtonClass: '<a class="btn btn-sm btn-primary uploader-save"  href="javascript:void(0)">',
+        saveButtonClass: 'btn btn-sm btn-primary',
         saveButtonText: '<i class="fa fa-save"></i></a>',
         removeButtonClass: 'btn btn-sm btn-danger',
         removeButtonText: '<i class="fa fa-remove"></i>',
-        uploaderClass: 'table',
+        uploaderClass: 'table table-striped table-condensed',
         commandRowClass: '',
         imageRowClass: '',
         imageContainerClass: '',
-        loadingImage: '<i class="fa fa-spin fa-spinner fa-3x"></i>',
+        loadingImage: '<div class="uploader-loading"><i class="fa fa-spin fa-spinner fa-3x"></i></div>',
         imageClass: 'img-responsive img-thumbnail',
         inputContainerClass: 'input-group input-group-sm',
         textInputClass: 'input-sm',
@@ -41,6 +41,145 @@
         set: function (settings) {
         }
     };
+
+    window.Uploader = function ($element, settings) {
+        var uc = UploaderConfig;
+
+        //create the table
+        var $table = $element
+            .append('<table>')
+            .children(':last-child')
+            .addClass(uc.uploaderClass)
+            .append('<tbody>')
+            .children(':last-child');
+
+        //create the command row
+        var $commandRow = $table.append('<tr>').children(':last-child');
+        $commandRow.addClass(uc.commandRowClass);
+        $commandRow.addClass('uploader-command-row');
+
+        //command buttons
+        $commandRow
+            .append('<td>')
+            .children(':last-child')
+            .css({width: '200px'})
+            .append('<a href="javascript:;" class="uploader-add">' + uc.addButtonText + '</a> &nbsp;')
+            .children(':last-child').addClass(uc.addButtonClass)
+            .end()
+            .append('<a href="javascript:;" class="uploader-save-all">' + uc.saveAllButtonText + '</a> &nbsp;')
+            .children(':last-child').addClass(uc.saveAllButtonClass)
+            .end()
+            .append('<a href="javascript:;" class="uploader-remove-all">' + uc.removeAllButtonText + '</a> &nbsp;')
+            .children(':last-child').addClass(uc.removeAllButtonClass)
+            .end();
+
+
+        var $add = $commandRow.find('.uploader-add');
+        var $saveAll = $commandRow.find('.uploader-save-all');
+        var $removeAll = $commandRow.find('.uploader-remove-all');
+        var rowCount = 0;
+
+        $add.click(function (e) {
+            //Creating the row,
+            var $newRow = $table.find('.uploader-command-row')
+                .after('<tr>')
+                .next('tr');
+
+            //add the columns needed
+            $newRow
+                .append('<td>')
+                .children(':last-child')
+                .append('<div class="loading">')
+                .children(':last-child')
+                .append(uc.loadingImage)
+                .end()
+                .append('<input type="file" class="hide"/>')
+                .end()
+                .append('<td>')
+                .children(':last-child')
+                .append('<a href="javascript:;" class="uploader-save"></a> &nbsp;')
+                .children(':last-child')
+                .addClass(uc.saveButtonClass)
+                .append(uc.saveButtonText)
+                .end()
+                .append('<a href="javascript:;" class="uploader-remove"></a>')
+                .children(':last-child')
+                .addClass(uc.removeButtonClass)
+                .append(uc.removeButtonText);
+
+
+            var inputs = '';
+
+            if (settings.inputs) {
+                for (c in settings.inputs) {
+                    var input = settings.inputs[c]
+
+                    switch (input.type) {
+                        case 'text':
+                            var $input = $newRow
+                                .append('<td>')
+                                .children(':last-child')
+                                .append('<div>')
+                                .children(':last-child')
+                                .addClass(uc.inputContainerClass)
+                                .append('<input type="text"/>')
+                                .children(':last-child')
+                                .addClass(uc.textInputClass);
+
+                            break;
+                    }
+
+                    $input.attr('id', input.name)
+                    $input.attr('name', input.name)
+                    if ($.isFunction(input.callback)) {
+                        input.callback($input);
+                    }
+                }
+            }
+
+            //create the handlers
+
+            //remove handler
+            $newRow.find('.uploader-remove').click(function () {
+                //cancelUpload();
+                $newRow.remove();
+                return false;
+            });
+
+            //when file is selected, que the image upload
+            $newRow.find('input[type=file]').change(function () {
+                //addToQueue();
+            });
+
+            //save handler
+            $newRow.find('.save').click(function () {
+                //saveUpload();
+            });
+
+            //execute preparations
+
+
+            return false;
+        });
+
+        $saveAll.click(function () {
+            //Go around clicking all save buttons
+            $table.find('.uploader-save').click();
+        });
+    };
+
+    $(document).ready(function () {
+        //create and attach an Uploader to the body
+        var u = new Uploader($('body').first(),
+            {
+                inputs: [{
+                    name: 'description', type: 'text', callback: function ($element) {
+                        console.log($element);
+                    }
+                }]
+            });
+
+    })
 
 
 }(jQuery));
